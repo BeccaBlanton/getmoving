14366f0b2ea0b697e76a7e093ef27587b0ddf508
Rebecca Blanton <beccaablanton@gmail.com>
2021-02-19 00:03:15 -0800

    :card_file_box: added database routes, html routes

---
+++ routes/routes.js
@@ -0,0 +1,67 @@
+const router = require("express").Router();
+const Workout = require('../models/workout.js')
+const path = require("path");
+
+router.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("/exercise", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+})
+
+router.get("/stats", (req, res)=>{
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+})
+
+router.post('/api/workouts', (req, res)=>{
+    Workout.create(req.body)
+    .then(dbWorkout => {
+        console.log(dbWorkout)
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+});
+  
+  router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+
+  router.get("/api/workouts/range", (req, res)=>{
+      Workout.find({})
+      .then(dbWorkout =>{
+          res.json(dbWorkout);
+      })
+      .catch(err => {
+          res.json(err);
+      })
+  })
+
+  router.put('/api/workouts/:id', (req, res)=> {
+      Workout.updateOne({_id: req.params.id},
+        { $push: {
+            workout: req.body
+        }
+    },
+    {
+        new: true,
+        runValidators: true
+    })
+    .then(dbWorkout =>{
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err)
+    })
+  })
+  
+  module.exports = router;
